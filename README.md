e5e62d48aa45743df0a129079ea9e828ecfc6252
YAO Cyrille <cyaoyapi@gmail.com>
2022-01-24 21:22:01 +0000

    Pull data from API

--- README.md
+++ README.md
@@ -3,6 +3,4 @@ Review react this tutorial : https://www.taniarascia.com/getting-started-with-re
 
 ## Summary
 
-  1. [Using React in HTML file with CDN links for React, ReactDOM and Babel](https://github.com/cyaoyapi/getting-started-with-react/tree/1-using-react-in-static-html-file)
-
-  2. [React with Create React App](https://github.com/cyaoyapi/getting-started-with-react/tree/2-create-react-app)
+Pull in api Data in React app.
---
+++ pull-in-api-data/src/App.js
@@ -0,0 +1,39 @@
+import {Component} from 'react';
+
+class App extends Component {
+
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*';
+
+    fetch(url).then((result) => {
+      return result.json();
+    }).then((result) => {
+      this.setState({
+        data: result,
+      })
+    })
+  }
+
+  render (){
+      const {data} = this.state;
+      const result = data.map((entry, index) => {
+        return <li key={index}>{entry}</li>;
+      })
+
+      return (
+        <div className="App">
+          <h1>Pull in data from Wikipedia API.</h1>
+          <ul>{result}</ul>
+        </div>
+      );
+  }
+
+    
+
+}
+
+export default App;
