0412172856d3ea6c8877f211d2b151ed7523648f
YAO Cyrille <cyaoyapi@gmail.com>
2022-01-24 16:58:17 +0000

    Function component

--- react-tutorial/src/components/Table.js
+++ react-tutorial/src/components/Table.js
@@ -1,30 +1,44 @@
 import {Component} from 'react';
 
+const TableHeader = function(){
+    return(
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+};
+
+const TableBody = () => {
+
+    return(
+        <tbody>
+            <tr>
+                <td>Dabira</td>
+                <td>Doctor</td>
+            </tr>
+            <tr>
+                <td>Akiva</td>
+                <td>God's servent.</td>
+            </tr>
+            <tr>
+                <td>Amram</td>
+                <td>Business man</td>
+            </tr>
+        </tbody>
+    );
+
+};
+
 class Table extends Component{
 
     render(){
         return (
             <table className='table'>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Dabira</td>
-                        <td>Doctor</td>
-                    </tr>
-                    <tr>
-                        <td>Akiva</td>
-                        <td>God's servent.</td>
-                    </tr>
-                    <tr>
-                        <td>Amram</td>
-                        <td>Business man</td>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody />
             </table>
         );
     }
